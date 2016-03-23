bfe384cfe2cda2ac6b195f845c8906fb95393845
Alex Yale-Loehr <ayaleloehr@gmail.com>
2016-03-23 15:51:15 -0400

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,18 +18,18 @@ EmploymentPortal::Application.routes.draw do
   get '/download_veterans' => 'employers#download_veterans'
   post '/download_candidate_veterans' => 'veterans#download_candidate_veterans'
   get "/favorites" => 'veterans#favorites'
-  get "/job-seekers/create-resume" => 'veterans#new', as: :resume_builder
-  get '/job-seekers/search-jobs' => 'search#search_jobs', as: :search_jobs
+  get "job-seekers/create-resume" => 'veterans#new', as: :resume_builder
+  get 'job-seekers/search_jobs' => 'search#search_jobs', as: :search_jobs
   post 'veterans/new' => 'veterans#new'
   get '/employers' => 'static_pages#employers', as: :employer_home
   get 'employer-list' => 'employers#index', as: :employer_list
   get '/download_all_veterans' => 'veterans#download_all_veterans'
-  get '/job-seekers/skills-translator' => 'skills#index', as: :skills_translator
-  post '/job-seekers/skills-translator/save_event' => 'skills#save_event'
-  post '/job-seekers/skills-translator/suggest' => 'skills#suggest'
-  get  '/job-seekers/skills-translator/get_skills/:prefix' => 'skills#get_skills', as: :skills_translator_get_skills
-  get  '/job-seekers/skills-translator/get_common_skills' => 'skills#get_common_skills'
-  post '/job-seekers/skills-translator/add_skill' => 'skills#add_skill'
+  get 'job-seekers/skills-translator' => 'skills#index', as: :skills_translator
+  post 'skills-translator/save_event' => 'skills#save_event'
+  post 'skills-translator/suggest' => 'skills#suggest'
+  get  'skills-translator/get_skills/:prefix' => 'skills#get_skills', as: :skills_translator_get_skills
+  get  'skills-translator/get_common_skills' => 'skills#get_common_skills'
+  post 'skills-translator/add_skill' => 'skills#add_skill'
   root 'static_pages#home'
-  match '/404' => 'errors#error404', via: [ :get, :post, :patch, :delete ]
+#  match '/404' => 'errors#error404', via: [ :get, :post, :patch, :delete ]
 end
