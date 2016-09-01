90d8e3749d3c1259ff2792984ac5f86ac7eca900
James Kassemi <jkassemi@gmail.com>
2016-09-01 17:30:12 -0600

    Increase timeout

--- lib/jobs_api.rb
+++ lib/jobs_api.rb
@@ -1,7 +1,7 @@
 class JobsApi
   include HTTParty
 
-  default_timeout 3  # Average response time is ~200-300ms
+  default_timeout 10  # Average response time is ~200-300ms
 
   SEARCH_URL = "#{ENV['JOBS_API_BASE_URL']}/search.json"
   MAX_ATTEMPTS = 2
