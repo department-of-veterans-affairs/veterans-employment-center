b818d239a3c3ab8830bfd0d15e40188cc522d11b
James Kassemi <jkassemi@users.noreply.github.com>
2016-09-01 17:40:41 -0600

    Retry requests to Jobs API on timeout (#359)
    * Retry requests to Jobs API on timeout

--- lib/jobs_api.rb
+++ lib/jobs_api.rb
@@ -1,7 +1,24 @@
 class JobsApi
   include HTTParty
 
+  default_timeout 10  # Average response time is ~200-300ms
+
+  SEARCH_URL = "#{ENV['JOBS_API_BASE_URL']}/search.json"
+  MAX_ATTEMPTS = 2
+
   def search(options)
-    self.class.get("#{ENV['JOBS_API_BASE_URL']}/search.json", options)
+    attempts = 0
+
+    begin
+      attempts += 1
+      self.class.get(SEARCH_URL, options)
+
+    rescue Timeout::Error
+      if attempts >= MAX_ATTEMPTS
+        raise
+      else
+        retry
+      end
+    end
   end
 end
