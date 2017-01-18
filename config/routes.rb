4e0f041ada4fb98bcc00df44517fec009cdfc7bb
Patrick Vinograd <patrick@adhocteam.us>
2017-01-18 14:18:30 -0800

    DoL redirects

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,10 @@ EmploymentPortal::Application.routes.draw do
   post 'veterans/new' => 'veterans#new'
   get 'employer-list' => 'employers#index', as: :employer_list
   root 'static_pages#home'
-  get '/employers' => redirect { |params, request| Rails.application.config.action_controller.relative_url_root }
+  get '/employers' => redirect('https://www.dol.gov/veterans/hireaveteran/') 
+  get '/job-seekers' => redirect('https://www.dol.gov/veterans/findajob/')
   get '/veterans' => redirect { |params, request| Rails.application.config.action_controller.relative_url_root }
   get '/job-seekers/skills-translator' => redirect { |params, request| Rails.application.config.action_controller.relative_url_root }
   get '/job-seekers/search-jobs' => redirect { |params, request| Rails.application.config.action_controller.relative_url_root }
-  get '/job-seekers' => redirect { |params, request| Rails.application.config.action_controller.relative_url_root }
   match '/404' => 'errors#error404', via: [ :get, :post, :patch, :delete ]
 end
